19d9f05f6bfd93de0d500ca489c7b94827e98a34
Resolviaai <resolviaai@gmail.com>
2025-08-21 19:59:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDoa8YzQ707xRs1IGD3AqdXNMgPwPM2FWA",
-    authDomain: "flowcheck-a4412.firebaseapp.com",
-    projectId: "flowcheck-a4412",
-    storageBucket: "flowcheck-a4412.firebasestorage.app",
-    messagingSenderId: "450070719140",
-    appId: "1:450070719140:web:52df78d02e59a45ba2d431"
+  apiKey: "AIzaSyDoa8YzQ707xRs1IGD3AqdXNMgPwPM2FWA",
+  authDomain: "flowcheck-a4412.firebaseapp.com",
+  projectId: "flowcheck-a4412",
+  storageBucket: "flowcheck-a4412.firebasestorage.app",
+  messagingSenderId: "450070719140",
+  appId: "1:450070719140:web:52df78d02e59a45ba2d431",
+  measurementId: "G-M74ZCJ9G53"
 };
 
 // Initialize Firebase
